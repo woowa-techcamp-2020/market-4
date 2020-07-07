ee070533184eddd2d542b7d3b7f228d2916c5be0
brightchul <abhbtbb@gmail.com>
2020-07-07 23:30:08 +0900

    [modify] 로그인 라우터 쿠키 활용 수정
    쿠키를 이용해서 로그인 반영하는 로직을 추가함.
    세션이 쿠키보다 더 낫다 생각되어 추후 변경할 예정임

--- routes/login.js
+++ routes/login.js
@@ -1,13 +1,30 @@
 const router = require('express').Router();
-const {db, memberDAO} = require('./db');
+const { db, memberDAO } = require('../db.js');
 
 
 router.get('/', (req, res, next) => {
-  res.send('respond with a resource');
+  let valid;
+  res.cookie("loginValid", false);
+  res.render('login');
 });
 
-router.post('/', (req,res,next) => {
+router.post('/', async (req, res, next) => {
+  const { userid, password } = req.body;
+  const result = await memberDAO.confirmUser(userid, password);
 
-})
+  if (!result) {
+    res.redirect('/login?isFail=true');
+  } else {
+    setCookie(res, result);
+    res.redirect('/');
+  }
+});
+
+function setCookie(res, result) {
+  res.cookie("userid", result.userid);
+  res.cookie("name", result.name);
+  res.cookie("email", result.email);
+  res.cookie("phone", result.phone);
+}
 
 module.exports = router;
