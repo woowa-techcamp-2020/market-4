ce04ecfd9c1307ad21c5019a9ebf46c086639d54
brightchul <abhbtbb@gmail.com>
2020-07-09 23:18:07 +0900

    [add] .travis.yml for travis ci

--- public/javascripts/signup/util.js
+++ public/javascripts/signup/util.js
@@ -21,6 +21,38 @@ const addError = function(border_ele, res, msg_ele) {
   }
 }
 
+const $ = selector => {
+  const elements = document.querySelectorAll(selector);
+  switch(elements.length) {
+      case 0 : return null;
+      case 1 : return elements[0];
+      default : return elements;
+  }
+}
+
+const getValueInCookie = key => {
+  const keyValue = document.cookie.split('; ').find( v => v.includes(key));
+  let value;
+  if(keyValue) {
+      value = keyValue.split('=')[1];
+  }
+  return value;
+}
+
+
+const debouncing = (callback, mSeconds) => {
+  let timer;
+  return (e) => {
+      if(!timer) {
+        timer = setTimeout(() => {
+          timer = null;
+          callback(e);
+        }, mSeconds);
+      }
+  }
+}
+
+
 export {
-  textMessage, errorElement, addError
+  textMessage, errorElement, addError, $, getValueInCookie, debouncing
 }
